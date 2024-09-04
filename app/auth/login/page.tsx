02dc64dac4e9818057024de3e7fbd6ba2b0dba87
Ming <147391099+Ming3210@users.noreply.github.com>
2024-09-04 11:31:23 +0700

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function page() {
+export default function Page() {
   const isCheckLogin = true;
   const router = useRouter();
   const login = () => {
